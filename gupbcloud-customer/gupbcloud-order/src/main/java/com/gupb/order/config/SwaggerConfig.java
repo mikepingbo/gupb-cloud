a150c92e13fb202190bf46e763e1d8c9b521a450
mikepingbo <15942801836@163.com>
2019-06-13 21:56:11 +0800

    swagger修改

--- gupbcloud-customer/gupbcloud-order/src/main/java/com/gupb/order/config/SwaggerConfig.java
+++ gupbcloud-customer/gupbcloud-order/src/main/java/com/gupb/order/config/SwaggerConfig.java
@@ -1,40 +1,40 @@
-package com.gupb.order.config;
+package com.chaselx.apidoc.config;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
+import static springfox.documentation.builders.PathSelectors.ant;
+
+/**
+ * @author ChaselX
+ * @date 2019/1/30 11:22
+ */
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket createRestApi() {
+    public Docket restApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.gupb.order"))
-                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
-                .paths(PathSelectors.any())
+                .paths(Predicates.and(ant("/**"), Predicates.not(ant("/error"))))
                 .build();
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("商城端Order服务接口文档")
-                .description("gupbcloud-order")
-                .contact(new Contact("gupb", "", "176826313@qq.com"))
-                .version("2.0")
+                .title("API Document")
+                .description("API Document for Spring Boot 2 Project")
+                .contact(new Contact("ChaselX", "", ""))
+                .version("1.0")
                 .build();
     }
 }
