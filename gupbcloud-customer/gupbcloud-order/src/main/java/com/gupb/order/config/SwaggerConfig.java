0611d6b81f1b3b16f99c31e92eafcf495740f46c
mikepingbo <15942801836@163.com>
2019-06-13 22:00:16 +0800

    swagger添加

--- gupbcloud-customer/gupbcloud-order/src/main/java/com/gupb/order/config/SwaggerConfig.java
+++ gupbcloud-customer/gupbcloud-order/src/main/java/com/gupb/order/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
-package com.chaselx.apidoc.config;
+package com.gupb.order.config;
 
 import com.google.common.base.Predicates;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -21,11 +25,14 @@ import static springfox.documentation.builders.PathSelectors.ant;
 public class SwaggerConfig {
 
     @Bean
-    public Docket restApi() {
+    public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .paths(Predicates.and(ant("/**"), Predicates.not(ant("/error"))))
+                .apis(RequestHandlerSelectors.basePackage("com.gupb.order"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
                 .build();
     }
 
