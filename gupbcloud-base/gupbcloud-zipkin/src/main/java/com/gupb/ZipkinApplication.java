5ada5bd0a8b106f9e0cff0d80fa5e214a04db993
gupb <gupb>
2019-06-29 17:00:00 +0800

    zipkin引入selaticsearch修改

--- gupbcloud-base/gupbcloud-zipkin/src/main/java/com/gupb/ZipkinApplication.java
+++ gupbcloud-base/gupbcloud-zipkin/src/main/java/com/gupb/ZipkinApplication.java
@@ -3,7 +3,7 @@ package com.gupb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import zipkin.server.internal.EnableZipkinServer;
+import zipkin.server.EnableZipkinServer;
 
 @EnableDiscoveryClient
 @SpringBootApplication
