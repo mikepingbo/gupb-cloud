c14ed1187c2cd1a65929a154f05e3e4d339c6d79
mikepingbo <15942801836@163.com>
2019-07-04 21:19:21 +0800

    zipkin添加kafka

--- gupbcloud-base/gupbcloud-zipkin/src/main/java/com/gupb/zipkin/ZipkinApplication.java
+++ gupbcloud-base/gupbcloud-zipkin/src/main/java/com/gupb/zipkin/ZipkinApplication.java
@@ -3,7 +3,7 @@ package com.gupb.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import zipkin.server.EnableZipkinServer;
+import zipkin2.server.internal.EnableZipkinServer;
 
 @EnableDiscoveryClient
 @SpringBootApplication
